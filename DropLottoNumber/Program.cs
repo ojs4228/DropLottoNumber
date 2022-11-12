84b7a8744720b53bec70ff54062e4c1a783a1ac2
korMons <79525540+ojs4228@users.noreply.github.com>
2022-11-12 15:39:54 +0900

    Add files via upload

---
+++ DropLottoNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DropLottoNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+
+            for (int i = 0; i <= 5; i++)
+            {
+               
+                Console.Write(random.Next(45) +" , ");
+                
+            }
+           
+        }
+    }
+}
